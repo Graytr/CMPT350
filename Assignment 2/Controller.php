8cc361797086fe46bfd4debcc07c62ada41c2050
tlg609 <tlg609@mail.usask.ca>
2016-02-27 15:28:36 -0600

    Changed controller to hopefully (donut emoji) connect to the database

--- Assignment 2/Controller.php
+++ Assignment 2/Controller.php
@@ -1,11 +1,54 @@
 <?php    
 
-if (!isset($_REQUEST['view'])){    
-	include 'view.php';
+
+if (!isset($_REQUEST['newBuilding'])){ 
+	//If the variables are all empty -> ie) this is the first call to the
+	//this file and we need to set up the forms to view the databases
+	 
+	$link = mysqli_connect('localhost', 'root', ''); //Connect to the database
+	
+	if (!$link)	{ 
+		$database = 'Unable to connect to the database server.'; 
+	} else {
+		//$database = the database
+		
+		//Set the character encoding to utf8 and kill myself if otherwise
+		if (!mysql_set_charset($link, 'utf8')){
+			$database = 'Unable to establish database encoding';
+			include 'view-buildings.php'
+			exit();
+		}
+		
+		//Grab the actual database
+		if( !mysql_select_db($link, 'database') ){
+			//Kill myself
+			$database = 'Unable to locate the database';
+			include 'view-buildings.php';
+			exit();
+		}
+		
+		
+		$database = 'We all good fam';
+		
+		
+	}  
+	
+	include 'view-buildings.php';
+	exit();
 } else {    
 
-  //Update database here with information
-  	include 'view.php';
+	//Update database here with information
+	
+	$link = mysqli_connect('localhost', 'root', ''); 
+	if (!$link)	{ 
+		$database = 'Unable to connect to the database server.'; 
+		exit(); 
+	} else {
+		//$database = the database using $_REQUEST['newBuilding'] and etc
+		exit();
+	}
+
+	include 'view-buildings.php';
 
 }    
 
