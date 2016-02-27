1d17f1e2ef18d2218017f3ed9f557a5903bbf539
hsj294 <hsj294@mail.usask.ca>
2016-02-27 17:31:34 -0600

    Select Data Files
    Made a php file for each table  that can be used to get the data out of
    the data

--- Assignment 2/MySQLi/select_data_city.php
+++ Assignment 2/MySQLi/select_data_city.php
@@ -5,13 +5,13 @@
 	$dbname = "myDatabase";
 	$conn = new mysqli($servername, $username, $password,$dbname);
 
-	$sql="SELECT firstname, lastname,email,comment FROM posts";
+	$sql="SELECT * FROM city";
 	$results=$conn->query($sql);
 	if($results->num_rows >0){
 		while($row=$result->fetch_assoc()){
 
 		}
 	}else{
-		echo "0 results";
+		echo "0 results in city table";
 	}
 ?>
