ab01d9fa31c41351b82a5bc0a8e44f6c7abd2ead
hsj294 <hsj294@mail.usask.ca>
2016-03-28 18:06:45 -0600

    Database
    Working on the Database for our final project

---
+++ Project/MySQLi/createDB.php
@@ -0,0 +1,23 @@
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	// Create connection
+	$conn = new mysqli($servername, $username, $password);
+// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+// Create database
+
+
+	$sql = "CREATE DATABASE IF NOT EXISTS myDatabase";
+	if ($conn->query($sql) === TRUE) {
+		//TESTING PURPOSES V
+		//echo "Database created successfully";
+	} else {
+		//TESTING PURPOSES V
+		//echo "Error creating database: " . $conn->error;
+	}
+	$conn->close();
+?>
