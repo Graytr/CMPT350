25d66dee78241d25cea59f738445c7de7a6d870d
tlg609 <tlg609@mail.usask.ca>
2016-04-02 16:34:51 -0600

    SignIn.js was made. Need to have a div called sign in, a p called UserInfo, and an a that looks like this:   <a id="SignOut" href="#" onclick="signOut();"></a>

---
+++ Project/SignIn.js
@@ -0,0 +1,35 @@
+function onSuccess(googleUser) {
+	console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+	console.log(googleUser.getBasicProfile().getName() + "\n" + googleUser.getBasicProfile().getEmail());
+	document.getElementById("UserInfo").innerHTML = googleUser.getBasicProfile().getName() + "\n" + googleUser.getBasicProfile().getEmail();
+	document.getElementById("SignOut").innerHTML = "Sign Out";
+
+	/*
+	Other options that we can get for googleUser.getBasicProfile():
+	.getId()
+	.getImageUrl()
+	*/
+}
+function onFailure(error) {
+	console.log(error);
+}
+function renderButton() {
+	gapi.signin2.render('SignInButton', {
+		'scope': 'profile email',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
+	});
+}
+function signOut() {
+	document.getElementById("UserInfo").innerHTML = "";
+	document.getElementById("SignOut").innerHTML = "";
+
+	var auth2 = gapi.auth2.getAuthInstance();
+	auth2.signOut().then(function () {
+		console.log('User signed out.');
+	});
+}
